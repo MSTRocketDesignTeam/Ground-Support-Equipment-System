565cc4c0a5d59d46d1a684e1938ac8415a4d02f6
CotterL1138 <Lucasc1138@gmail.com>
2026-03-07 12:45:29 -0600

    added stop button

--- coldFlowTestingSW/coldFlowTest.py
+++ coldFlowTestingSW/coldFlowTest.py
@@ -23,35 +23,45 @@ time.sleep(2) #Let device initialize
 ser.write(b"FF")   
 ser.flush()
 
+
 def runFuel():
+	if running:
+		return
+	running = True
 	openTime = int(runTime.get(1.0, "end-1c"))
 	ser.write(b"TF")
 	ser.flush()
-	time.sleep(openTime)
-	ser.write(b"FF")
-	ser.flush()
+	time.after(openTime, closeValves)
 	
 def runOx():
+	if running:
+		return
+	running = True
 	openTime = int(runTime.get(1.0, "end-1c"))
 	ser.write(b"FT")
 	ser.flush()
-	time.sleep(openTime)
-	ser.write(b"FF")
-	ser.flush()
+	time.after(openTime, closeValves)
 
 def runBoth():
+	if running:
+		return
+	running = True
 	openTime = int(runTime.get(1.0, "end-1c"))
 	ser.write("TT".encode('utf-8'))
 	ser.flush()
-	time.sleep(openTime)
+	time.after(openTime, closeValves)
+
+def closeValves():
 	ser.write(b"FF")
 	ser.flush()
+	running = False
 
 
 root = tk.Tk()
 root.geometry('700x700')
 root.resizable(True, True)
 root.title('Cold Flow GUI')
+ttk.style().configure("stop.button", background="red",foreground="white", font=("Arial", 16, "bold"))
 
 #RDT Logo initialization
 rdtLogo = Image.open(r"../assetsAndImages/RDT_LOGO.png")
@@ -70,6 +80,7 @@ runTime.pack()
 runFuelButton = ttk.Button(root, text = "Run Fuel Line Cold Flow", command=lambda: runFuel())
 runOxButton = ttk.Button(root, text = "Run Ox Line Cold Flow", command=lambda: runOx())
 runBothLinesButton = ttk.Button(root, text = "Run Both Lines Cold Flow", command=lambda: runBoth())
+stopButton = ttk.Button(root, text = "STOP", command=lambda: closeValves(), style = "stop.button" )
 
 #Arrange all widgets in grid
 runTimeFrame.grid(row=3, column=1, padx=10, pady=10)
